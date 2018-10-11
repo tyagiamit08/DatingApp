3d60bf9cbf870c5b1d83e5c3de1aeff67e2a7694
Amit Tyagi <amit.tyagi@external.mckinsey.com>
2018-10-11 10:31:24 +0530

    Adding Project with Getting sample data using api from SQL lite database

---
+++ DatingApp.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DatingApp.WebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public ValuesController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            return Ok(await _dataContext.Values.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await _dataContext.Values.FirstOrDefaultAsync(x => x.Id == id));
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
