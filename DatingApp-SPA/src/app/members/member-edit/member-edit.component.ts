2b8629c24ee9e09075913b7ed3c7324fdf4ffe1d
Amit Tyagi <amit.tyagi@external.mckinsey.com>
2018-11-21 09:20:52 +0530

    Added Memeber Edit Component and Resolver and DTO for the same

---
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
+import { User } from 'src/app/_models/User';
+import { ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css']
+})
+export class MemberEditComponent implements OnInit {
+  user: User;
+  @ViewChild('editForm') editForm: NgForm;
+  @HostListener('window:beforeunload', ['$event'])
+  unloadNotification($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
+
+  constructor(
+    private route: ActivatedRoute,
+    private alertify: AlertifyService
+  ) {}
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.user = data['user'];
+    });
+  }
+
+  updateUser() {
+    console.log(this.user);
+    this.alertify.success('Profile has been updated successfully');
+    this.editForm.reset(this.user);
+  }
+}
---
+++ DatingApp.WebAPI/DTOs/userToUpdateDTO.cs
@@ -0,0 +1,11 @@
+namespace DatingApp.WebAPI.DTOs
+{
+    public class userToUpdateDTO
+    {
+        public string Introduction { get; set; }
+        public string Interests { get; set; }
+        public string LookingFor { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
